2f38b0ab6098940ef9a0c32e365ed239e3e09aac
John Doe <hamedhasan422421@gmail.com>
2023-07-02 16:23:40 +0300

    updated package.json

--- mail/mailer.js
+++ mail/mailer.js
@@ -13,14 +13,35 @@ const transporter = nodemailer.createTransport({
 // Function to send the email
 const sendEmail = async (name, email, subject, message) => {
   try {
-    console.log('Sender email:', email);
+    console.log(email)
     // Compose the email
     const mailOptions = {
-      from: 'swe.hamedhasan@gmail.com',
+      from: email,
       to: 'swe.hamedhasan@gmail.com', // Update the recipient email address here
       replyTo: email, // Set the reply-to address to the user's email
       subject: subject,
-      text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+      html: `
+        <div style="background: linear-gradient(135deg, #22d3ee 0%, #bfdbfe 100%); padding: 1rem; border-radius: 10px;">
+          <div style="background: linear-gradient(135deg, #f59e0b 0%, #fdba74 100%); padding: 1.5rem; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">
+            <div style="margin-bottom: 1rem;">
+              <h3 style="font-size: 1.5rem; font-weight: bold; color: #171717;">Name:</h3>
+              <p style="margin: 0;">${name}</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <h3 style="font-size: 1.5rem; font-weight: bold; color: #171717;">Email:</h3>
+              <p style="margin: 0;">${email}</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <h3 style="font-size: 1.5rem; font-weight: bold; color: #171717;">Subject:</h3>
+              <p style="margin: 0;">${subject}</p>
+            </div>
+            <div style="margin-bottom: 1rem;">
+              <h3 style="font-size: 1.5rem; font-weight: bold; color: #171717;">Message:</h3>
+              <p style="margin: 0;">${message}</p>
+            </div>
+          </div>
+        </div>
+      `,
     };
 
     // Send the email using the transporter
